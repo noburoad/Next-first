227834f39a7c9373f4d1521a8f5d0075d4cd56d1
ノブ <>
2021-09-29 12:59:30 +0900

    swr実装中

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import Header from './components/header';
 import Content from './components/content';
+import useSWR from 'swr';
 
 export default function Home() {
+
+	let practice = '練習'
+	const fetcher = () => fetch('/api/message')
+	const {data, error} = useSWR('/api/message', fetcher)
+	if(error) return <div>failed to load</div>
+	if(!data) return <div>loading...</div>
+	console.log(data.message)
 	return (
 		<>
-			<Header title={`ヘッダー`}></Header>
+			
 			<Content>
+			<Header title={ practice } />
 				<h2 style={{ textAlign: 'center' }}>初めてのNext.jsだよ</h2>
-				<p style={{ textAlign: 'center' }}>べんきょうちゅう</p>
+				<p style={{ textAlign: 'center' }}> { data.message } </p>
 			</Content>
 		</>
 	);
